a3e836fe744db5d0b0e4a6aea3949bc6507bc7d9
Andres Betancourt <andres11152@gmail.com>
2024-10-28 20:32:02 -0500

    changes in index

--- scriptNuevo.js
+++ scriptNuevo.js
@@ -59,5 +59,15 @@ document.addEventListener('visibilitychange', function() {
 });
 
 
+/*===== Cambio de idioma =====*/
+const changeLanguage = async language => {
+    const texts = await fetch(`./languages/${language}.json`).then(res => res.json());
+    textsToChange.forEach(textToChange => {
+        const { section, value } = textToChange.dataset;
+        textToChange.innerHTML = texts[section][value];
+    });
+};
+
+flagsElement.addEventListener('click', e => changeLanguage(e.target.parentElement.dataset.language));
 
 
